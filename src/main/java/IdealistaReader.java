c2f9fa8a74e9f632636dc57e2c6ae551014602fc
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2021-05-01 18:41:06 +0200

    Added utils to read idealista file

---
+++ src/main/java/IdealistaReader.java
@@ -0,0 +1,53 @@
+import scala.Tuple2;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * To avoid hardcoded path will be used this class that contains utility methods that can read "idealista" folder.
+ */
+public class IdealistaReader {
+
+    public static final String FILE_SEPARATOR = File.separator;
+    private static final String IDEALISTA_FOLDER = "src_main_resources_idealista".replace("_",FILE_SEPARATOR);
+
+    public static Stream<String> allDates() {
+        return Arrays.stream(new File(IDEALISTA_FOLDER).list())
+                .map(s -> s.split("_idealista")[0])
+                .sorted();
+    }
+
+    public static Stream<String> allFilesName() {
+        return allDates()
+                .map(e -> filePerDate(e))
+                .filter(e -> e.isPresent())
+                .map(e -> e.get());
+    }
+
+    public static Optional<String> filePerDate(final String date) {
+        return Arrays.stream(new File(IDEALISTA_FOLDER + FILE_SEPARATOR + date + "_idealista").list())
+                .filter(s -> s.endsWith(".parquet"))
+                .findFirst();
+    }
+
+    public static Stream<String> allFilesPath() {
+        return allDates()
+                .map(date -> new Tuple2<>(date, filePerDate(date)))
+                .filter(t -> t._2.isPresent())
+                .map(t -> IDEALISTA_FOLDER + FILE_SEPARATOR + t._1 + FILE_SEPARATOR + t._2.get());
+    }
+
+    public static Stream<Tuple2<String,String>> allPairDateFilePath() {
+        return allDates()
+                .map(date -> new Tuple2<>(date, filePerDate(date)))
+                .filter(t -> t._2.isPresent())
+                .map(t -> new Tuple2<>(t._1, IDEALISTA_FOLDER + FILE_SEPARATOR + t._1 + FILE_SEPARATOR + t._2.get()));
+    }
+
+
+
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,34 @@
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
 public class Main {
 
-	static final String APPLICATION_NAME = "BDM_P2";
-	
+	static final String APP_NAME = "BDM_P2";
+	static final String PARQUET_FILE = "src/main/resources/idealista/2020_01_02_idealista/part-00000-73a2bf77-0d24-4f05-bf18-7351de8d7938-c000.snappy.parquet";
+	static final SparkSession spark = SparkSession
+			.builder()
+			.appName(APP_NAME)
+			.master("local[*]")
+			.getOrCreate();
+
 	public static void main(String[] args) throws Exception {
 
-		SparkConf conf = new SparkConf().setAppName(APPLICATION_NAME).setMaster("local[*]");
-        JavaSparkContext ctx = new JavaSparkContext(conf);
+		IdealistaReader.allFilesName().forEach(f -> System.out.println(f));
+		IdealistaReader.allFilesPath().forEach(f -> System.out.println(f));
+		IdealistaReader.allPairDateFilePath().forEach(f -> System.out.println(f));
 
-		ReadFile.basicAnalysis(ctx);
+
+
+		// Try to read and clean data separately at first
+		JavaRDD<Row> parquetFileDF = spark.read().parquet(PARQUET_FILE).javaRDD();
+		parquetFileDF
+				.mapToPair(e -> new Tuple2<>("THE DATE",e))
+				.foreach(e -> System.out.println(e));
 
 	}
+
+
 }
 
