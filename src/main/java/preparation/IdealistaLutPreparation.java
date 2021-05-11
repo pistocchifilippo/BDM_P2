f7236fcae67aa692c60505b93423a715d5fa3452
AinaMontalban <aina.montalban@alum.esci.upf.edu>
2021-05-11 21:29:26 +0200

    Added Idealista Lookup Preparation

---
+++ src/main/java/preparation/IdealistaLutPreparation.java
@@ -0,0 +1,51 @@
+package preparation;
+
+import com.mongodb.spark.MongoSpark;
+import com.mongodb.spark.config.ReadConfig;
+import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.bson.Document;
+import scala.Tuple2;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IdealistaLutPreparation implements Preparation <String,String>{
+
+    private final String path;
+
+    public IdealistaLutPreparation(final String path) {
+        this.path = path;
+    }
+
+    @Override
+    public JavaPairRDD<String, String> prepare(final SparkSession spark) {
+
+
+        // Create a JavaSparkContext using the SparkSession's SparkContext object
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+        Map<String, String> readOverrides = new HashMap<String, String>();
+        readOverrides.put("database", "lookup_tables");
+        readOverrides.put("collection", "rent_lut_neigh");
+        readOverrides.put("readPreference.name", "secondaryPreferred"); // mirar millor (!!!)
+        ReadConfig readConfig = ReadConfig.create(jsc).withOptions(readOverrides);
+
+
+        JavaMongoRDD<Document> rdd = MongoSpark.load(jsc);
+        System.out.println(rdd.take(5));
+        JavaPairRDD<String, String> rdd2 = rdd.mapToPair((document) ->{
+
+            String neigh = document.getString("neighborhood");
+            String id = document.getString("_id");
+
+            return new Tuple2<String, String>(neigh, id);
+
+        });
+
+        return rdd2;
+    }
+}
