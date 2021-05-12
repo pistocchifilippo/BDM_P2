5a87cbf09ab2e13211a7e80af669b5ac763dcce4
AinaMontalban <aina.montalban@alum.esci.upf.edu>
2021-05-12 22:14:35 +0200

    Cleaned Income Lut Preparation

--- src/main/java/preparation/IncomeLutPreparation.java
+++ src/main/java/preparation/IncomeLutPreparation.java
@@ -1,6 +1,7 @@
 package preparation;
 
 import com.mongodb.spark.MongoSpark;
+import com.mongodb.spark.config.ReadConfig;
 import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -8,7 +9,9 @@ import org.apache.spark.sql.SparkSession;
 import org.bson.Document;
 import scala.Tuple2;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class IncomeLutPreparation implements Preparation <String,String>{
 
@@ -24,14 +27,19 @@ public class IncomeLutPreparation implements Preparation <String,String>{
         // Create a JavaSparkContext using the SparkSession's SparkContext object
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
+        Map<String, String> readOverrides = new HashMap<String, String>();
+        readOverrides.put("database", "lookup_tables");
+        readOverrides.put("collection", "income_lut_neigh");
+        readOverrides.put("readPreference.name", "secondaryPreferred"); // mirar millor (!!!)
+        ReadConfig readConfig = ReadConfig.create(jsc).withOptions(readOverrides);
+
+
         JavaMongoRDD<Document> rdd = MongoSpark.load(jsc);
-        System.out.println(rdd.take(5));
+
         JavaPairRDD<String, String> rdd2 = rdd.mapToPair((document) ->{
             String neigh = document.getString("neighborhood");
             String id = document.getString("_id");
-            //String name = document.getString("neighborhood_name");
-            //String re = document.getString("neighborhood_reconciled");
-            //String value = neigh + ";" + id + ";" + name + ";" + re;
+
 
             return new Tuple2<String, String>(neigh, id);
 
