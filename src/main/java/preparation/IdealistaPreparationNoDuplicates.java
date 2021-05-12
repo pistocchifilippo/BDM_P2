5bab02eee53717d9db50f7d072a8a1b90b4d99fa
pistocchifilippo <filippo.pistocchi4@studio.unibo.it>
2021-05-12 18:26:53 +0200

    Filtering on null, true and false strings

--- src/main/java/preparation/IdealistaPreparationNoDuplicates.java
+++ src/main/java/preparation/IdealistaPreparationNoDuplicates.java
@@ -5,11 +5,12 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import scala.Serializable;
 import scala.Tuple2;
 
 import java.util.stream.Stream;
 
-public class IdealistaPreparationNoDuplicates implements Preparation <String,String>{
+public class IdealistaPreparationNoDuplicates implements Preparation<String, String> {
 
     private static final int NEIGHBOURHOOD = 20;
     private static final int PROPERTY_ID = 28;
@@ -32,8 +33,10 @@ public class IdealistaPreparationNoDuplicates implements Preparation <String,Str
                         t._2.map(r -> t._1 + SEPARATOR + r.mkString(SEPARATOR))
                 )
                 .reduce(emptyRDD, (a,b) -> a.union(b))
+                .filter(e -> !e.split(SEPARATOR)[NEIGHBOURHOOD].equals("null") && !e.equals("true") && !e.equals("false"))
                 .mapToPair(l -> new Tuple2<>(l.split(SEPARATOR)[PROPERTY_ID], l))
                 .reduceByKey((String a, String b) -> (a.split(SEPARATOR)[0].compareTo(b.split(SEPARATOR)[0])) < 0 ? a : b)
                 .mapToPair(l -> new Tuple2<>(l._2.split(SEPARATOR)[NEIGHBOURHOOD], l._2));
     }
+
 }
